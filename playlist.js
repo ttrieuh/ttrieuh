8a5ac2a427d1c777897361e7532fbd0bde170f6b
ttrieuh <139837664+ttrieuh@users.noreply.github.com>
2025-04-14 22:25:30 +0700

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -6,7 +6,11 @@
   {
     "name": "rocky",
     "url": "https://rr3---sn-8pxuuxa-nbozl.googlevideo.com/videoplayback?expire=1744664678&ei=BiT9Z4ngGMm1ir4PzZvXgAI&ip=2a09%3Abac1%3A50e0%3A1780%3A%3Af%3A18e&id=o-ANI5h-rhzyt1uCv6KC8LZpSHrCVcGYJyZ1n6viYVwHPy&itag=140&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&rms=au%2Cau&bui=AccgBcPmzh1Pi38rRMWWldHDA2k7WVVUGg-7AY8SPXTrFAq2zJBXoAB_9obUNGXYnJq6bXQSpKLBmamq&vprv=1&svpuc=1&mime=audio%2Fmp4&ns=kZo6uBlbJfr2Bk29AMf4BhsQ&rqh=1&gir=yes&clen=2386291&dur=147.400&lmt=1744437936469764&keepalive=yes&lmw=1&c=TVHTML5&sefc=1&txp=6308224&n=9yKCxpK63M-jmQ&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cbui%2Cvprv%2Csvpuc%2Cmime%2Cns%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRQIhAJPvBI51PnBvOwPaZABNavb_XMYV1J2i0-YIK4iO-8ddAiB7GOZapMYD_62mK-DRKjrA7LORECkJmhAdgioUxJA6FQ%3D%3D&title=Gi%E1%BB%AF+L%E1%BA%A5y+L%C3%A0m+G%C3%AC+-+MONSTAR+%28pord.+orizz%29+Drill+-+TIKTOK+VER+%28Remake%29&redirect_counter=1&rm=sn-q4fel77l&rrc=104&fexp=24350590,24350737,24350827,24350961,24351063,24351173,24351230,24351523,24351528,24351545,24351606,24351637,24351672&req_id=dc4b90120297a3ee&cms_redirect=yes&cmsv=e&ipbypass=yes&met=1744643088,&mh=Im&mip=2402:800:6371:417f:8198:928a:a4fe:2cbb&mm=31&mn=sn-8pxuuxa-nbozl&ms=au&mt=1744641911&mv=u&mvi=3&pcm2cms=yes&pl=48&lsparams=ipbypass,met,mh,mip,mm,mn,ms,mv,mvi,pcm2cms,pl,rms&lsig=ACuhMU0wRAIgWfZo0kKuLYegbhn1aMRzKBWUUI-MFqXCFcHI-rkLw48CIHvB0X7eZM7Blc1ppPFX-rzWs0vRnYV45EfgsYPDLKIl"
-  }
+  },
+    {
+        "name": "nhacda",
+        "url": "https://drive.google.com/uc?export=download&id=1W9tPzWPFJ328jhFne825Rkx0HCGJHYI-"
+    }
 
   ]
   
