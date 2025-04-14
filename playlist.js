cfeec668d9b8f77863b1589a9634004ea7e106c2
ttrieuh <139837664+ttrieuh@users.noreply.github.com>
2025-04-14 21:20:47 +0700

    Create playlist.js

---
+++ playlist.js
@@ -0,0 +1,7 @@
+[
+    {
+      "name": "Bê Trap #1 (Trap heo quay)",
+      "url": "https://rr4---sn-npoe7ne7.googlevideo.com/videoplayback?expire=1744660796&ei=3BT9Z9HYK8ubhcIP89OVoQo&ip=104.28.197.140&id=o-AL-875jZHPTeLanSjNSz9qNXwDCjxsZTjrUGdDCbgg7v&itag=140&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&bui=AccgBcPNRAbJrSz4NwxH_gvGF5qq3pof3SQKL3Fmyo4AWD49ISuMBNhzppBul8r5F1BIG_SvwnUSFXuq&vprv=1&svpuc=1&mime=audio%2Fmp4&ns=LdDUmX-ag5OLnu7hEOtGsCUQ&rqh=1&gir=yes&clen=16154426&dur=998.132&lmt=1742129289245504&keepalive=yes&lmw=1&c=TVHTML5&sefc=1&txp=5532534&n=X1Ofm4RE7ZjPSw&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cbui%2Cvprv%2Csvpuc%2Cmime%2Cns%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRgIhALPQBSBio5sVgXJ2jwoCd2P8dEl45C5SeH01cTKX9XoGAiEAuqCzaY6BXojsUzZYZcxSDXHHSXevRneAagh77-i35Kk%3D&title=B%C3%8A+TRAP+%231+%28+Trap+heo+quay+%29"
+    }
+  ]
+  
